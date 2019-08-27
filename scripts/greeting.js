e9dfda40a59688ebf8cf2c72b93d45e328b51144
rolandodelgado <44987524+rolandodelgado@users.noreply.github.com>
2019-08-27 15:29:21 -0400

    Update greeting.js

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -4,29 +4,29 @@ const greeting = document.querySelector('js-greetings');
 const USER_LS = 'currentUser';
 const SHOWING_CN = 'showing';
 
-function saveName(text){
+function saveName(text) {
     localStorage.setItem(USER_LS, text);
 }
 
-function handleSubmit(event){
+function handleSubmit(event) {
     event.preventDefault();
     const currentValue = input.value;
     paintGreeting(currentValue);
     saveName(currentValue);
 }
 
-function askForName(){
+function askForName() {
     form.classList.add(SHOWING_CN);
     form.addEventListener('submit', handleSubmit);
 }
 
-function paintGreeting(text){
+function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hola! ${text}`;
 }
 
-function loadName(){
+function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
         askForName();
@@ -35,7 +35,7 @@ function loadName(){
     }
 }
 
-function init(){
+function init() {
     loadName();
 }
 
