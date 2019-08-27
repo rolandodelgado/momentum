7c53b8adae8c01438a6e920369b5c386fca39898
rolandodelgado <44987524+rolandodelgado@users.noreply.github.com>
2019-08-27 14:45:24 -0400

    Update greeting.js

---
+++ scripts/greeting.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('.js-form');
+const input = form.querySelector('input');
+const greeting = document.querySelector('js-greetings');
+const USER_LS = 'currentUser';
+const SHOWING_CN = 'showing';
+  
