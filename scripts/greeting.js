1f26e0681f060fa9e463f3b33cd6548336d95330
rolandodelgado <44987524+rolandodelgado@users.noreply.github.com>
2019-08-27 14:52:22 -0400

    Update greeting.js

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -3,4 +3,18 @@ const input = form.querySelector('input');
 const greeting = document.querySelector('js-greetings');
 const USER_LS = 'currentUser';
 const SHOWING_CN = 'showing';
-  
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null) {
+        askForName();
+    } else {
+        paintGreeting(currentUser);
+    }
+}
+
+function init() {
+    loadName();
+}
+
+init();
